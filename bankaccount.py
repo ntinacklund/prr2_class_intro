38e6b218dec316139fd0be95d337a2c2132d34f5
Niclas L <niclas.lund@ntig.se>
2024-08-27 09:19:14 +0200

    Assignments added.

---
+++ bankaccount.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    
+    def __init__(self) -> None:
+        # Konstruktorn för ett bankkonto.
+        # Tar in variablerna account_number (str) och balance (int)
+        # Balance är int för att det ska lagras ören.
+        # Varför då?
+        pass
+    
+    def __str__(self) -> str:
+        # Strängrepresentation för ett bankkonto.
+        pass
+    
+    def deposit(self, amount: int):
+        # Lägg till pengar på saldot.
+        pass
+    
+    def withdraw(self, amount: int):
+        # Ta bort pengar från saldot.
+        pass
+    
+def main():
+    # Skapa några konton och testa att din klassimplementation fungerar.
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ person.py
@@ -0,0 +1,29 @@
+class Person:
+    
+    def __init__(self) -> None:
+        # Konstruktorn till Person
+        # tar in variablerna name, age och email
+        pass
+    
+    def __str__(self) -> str:
+        # Representera objektet som en sträng
+        pass
+    
+    def greet(self) -> str:
+        # Returnera en hälsningssträng
+        # Ex 'hej jag heter x och är y år gammal.
+        pass
+    
+    def has_birthday(self):
+        # Ökar på age med 1 för objektet
+        pass
+    
+def main():
+    # Skapa tre personer du lägger i en lista.
+    # Stega igenom listan och testa alla dina metoder.
+    # Om någon metod ändrar ett attribut, kontrollera detta
+    # när du stegar igenom.
+    pass
+
+if __name__ == "__main__":
+    main()
